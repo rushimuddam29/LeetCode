3997cf97b08434a9f9585a7b29c7dd15125dba40
rushimuddam29 <145119960+rushimuddam29@users.noreply.github.com>
2024-08-12 15:24:47 +0530

    Time: 5 ms (99.92%), Space: 45.8 MB (71.85%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -7,10 +7,7 @@ class Solution {
         int right = n-1;
         
         while(left<m && right>=0){
-            //int mid = (left+right)/2;
-            //int r = mid/n;
-            //int c = mid%n;
-            //int temp = matrix[r][c];
+          
             if(matrix[left][right] == target) return true;
             else if(matrix [left][right] < target)  left++;
             else right--;
